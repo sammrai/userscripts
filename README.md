bb9d6fa36714f1727602e0e773c2263e8e9f2998
saku <saku0aab@gmail.com>
2023-05-04 01:35:38 +0900

    Update userscript to improve browser compatibility and enhance README documentation

---
+++ README.md
@@ -0,0 +1,38 @@
+# YouTube Ad Skipper
+
+A simple userscript to automatically skip YouTube ads and control playback speed. Primarily tested on Safari.
+
+## Description
+
+This userscript automatically skips YouTube ads and provides a playback speed control to the user interface. It is not an ad-blocker but provides a more seamless experience when watching videos on YouTube. The playback speed control allows users to easily change the playback speed of the video without needing to open the video settings.
+
+**Note: This userscript has been primarily tested on Safari. While it may work on other browsers, compatibility is not guaranteed.**
+
+## Features
+
+- Automatically skips ads on YouTube
+- Provides a playback speed control for easy adjustment of video speed
+
+## Installation
+
+To use this userscript, you need to have a userscript manager extension installed in your browser such as Tampermonkey, Greasemonkey, or Violentmonkey.
+
+1. Install a userscript manager extension for your browser:
+    - [Tampermonkey for Chrome, Microsoft Edge, Safari, Opera, and Firefox](https://www.tampermonkey.net/)
+    - [Greasemonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+    - [Violentmonkey for Chrome, Microsoft Edge, and Firefox](https://violentmonkey.github.io/get-it/)
+2. Click on the following link to install the userscript: [YouTube Ad Skipper](https://github.com/sammrai/userscripts/raw/main/youtube_ad_skipper.user.js)
+3. Confirm the installation when prompted by your userscript manager.
+4. Refresh any open YouTube pages or navigate to YouTube to start using the userscript.
+
+## Usage
+
+Once installed, the userscript will automatically skip ads on YouTube videos. Additionally, a playback speed control will be displayed on the video player, allowing you to easily adjust the playback speed of the video.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Acknowledgments
+
+This userscript is based on the original work by [Vyasdev217](https://github.com/Vyasdev217/userscripts), which is licensed under the [MIT License](https://opensource.org/licenses/MIT).
--- youtube_ad_skipper.user.js
+++ youtube_ad_skipper.user.js
@@ -1,50 +1,68 @@
 // ==UserScript==
 // @name         YouTube ad skipper
 // @namespace    https://github.com/sammrai
-// @version      0.1
+// @version      0.2
 // @description  Simple ad skipper for youtube (This is not ad blocker)
 // @author       sammrai
 // @match        https://www.youtube.com/*
 // @grant        none
 // @license      MIT
 // @downloadURL  https://github.com/sammrai/userscripts/raw/main/youtube_ad_skipper.user.js
-// @updateURL	 https://github.com/sammrai/userscripts/raw/main/youtube_ad_skipper.user.js
+// @updateURL    https://github.com/sammrai/userscripts/raw/main/youtube_ad_skipper.user.js
 // ==/UserScript==
+
+// Based on the original work by Vyasdev217 (https://github.com/Vyasdev217/userscripts)
+// Licensed under the MIT License
 
 (function() {
     'use strict';
 
-    var pbs = document.createElement("INPUT");
-    pbs.type="number";
-    pbs.style="background-color: black;color: white;background-repeat:no-repeat;border: none;cursor:pointer;overflow: hidden;outline:none;width:8vw;text-align: center;font-size:auto;";
-    pbs.step=0.1;
-    pbs.min=0;
-    pbs.value=1.0;
-    pbs.addEventListener("change",function(){if(pbs.value>0){document.getElementsByTagName("video")[0].playbackRate = pbs.value;}else{pbs.value=1;document.getElementsByTagName("video")[0].playbackRate = pbs.value;}});
+    function createPlaybackSpeedControl() {
+        const playbackSpeedControl = document.createElement("input");
+        playbackSpeedControl.type = "number";
+        playbackSpeedControl.style = "background-color: black;color: white;background-repeat:no-repeat;border: none;cursor:pointer;overflow: hidden;outline:none;width:8vw;text-align: center;font-size:auto;";
+        playbackSpeedControl.step = 0.1;
+        playbackSpeedControl.min = 0;
+        playbackSpeedControl.value = 1.0;
 
-    function appendPlaybackControl() {
-        var centerElement = document.getElementById('movie_player') || document.getElementById('center');
+        playbackSpeedControl.addEventListener("change", function() {
+            const videoElement = document.getElementsByTagName("video")[0];
+            videoElement.playbackRate = Math.max(0.1, playbackSpeedControl.value);
+        });
+
+        return playbackSpeedControl;
+    }
+
+    function appendPlaybackControl(control) {
+        const centerElement = document.getElementById('movie_player') || document.getElementById('center');
+
         if (centerElement) {
-            centerElement.appendChild(pbs);
+            centerElement.appendChild(control);
         } else {
-            setTimeout(appendPlaybackControl, 1000);
+            setTimeout(() => appendPlaybackControl(control), 1000);
         }
     }
 
-    appendPlaybackControl();
+    function observeAndSkipAds() {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(() => {
+                const adSkipButton = document.querySelector('.ytp-ad-skip-button.ytp-button');
+                if (adSkipButton) {
+                    adSkipButton.click();
+                }
+                const adOverlayCloseButton = document.querySelector('.ytp-ad-overlay-close-button');
+                if (adOverlayCloseButton) {
+                    adOverlayCloseButton.click();
+                }
+            });
+        });
 
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-          if (document.contains(document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0])) {
-              document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0].click();
-          }
-          if (document.contains(document.getElementsByClassName('ytp-ad-overlay-close-button')[0])) {
-              document.getElementsByClassName('ytp-ad-overlay-close-button')[0].click();
-          }
-          document.getElementsByTagName("video")[0].playbackRate = pbs.value;
-      });
-    });
-    const config = {childList:true,subtree:true};
-    observer.observe(document.body, config);
+        const config = { childList: true, subtree: true };
+        observer.observe(document.body, config);
+    }
+
+    const playbackSpeedControl = createPlaybackSpeedControl();
+    appendPlaybackControl(playbackSpeedControl);
+    observeAndSkipAds();
 
 })();
